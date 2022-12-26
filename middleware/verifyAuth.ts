251972e1f0525ab430049667c7901f3a8f4d68fb
rhaix23 <patrick.lloyd.santos@gmail.com>
2022-12-26 18:10:17 +0800

    Auth and Role Verification Middleware Updated

--- middleware/verifyAuth.ts
+++ middleware/verifyAuth.ts
@@ -1,26 +1,32 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import { UnAuthenticatedError } from "../errors/unauthenticatedError.js";
+import { ForbiddenError } from "../errors/forbiddenError.js";
 
 const verifyAuth = (req: Request, res: Response, next: NextFunction) => {
   const cookies = req.cookies;
+  const accessToken = cookies.accessToken;
 
   if (!cookies.accessToken) {
-    throw new UnAuthenticatedError("Unauthorized");
+    throw new UnAuthenticatedError("Token not found");
   }
-
-  const token = cookies.accessToken;
 
   try {
     const decoded = jwt.verify(
-      token,
+      accessToken,
       process.env.JWT_TOKEN_SECRET as string
     ) as JwtPayload;
+
     req.user = {
       userId: decoded.userId,
       username: decoded.username,
       role: decoded.role,
+      isDisabled: decoded.isDisabled,
     };
+
+    if (req.user.isDisabled) {
+      throw new ForbiddenError();
+    }
 
     next();
   } catch (error) {
--- middleware/verifyRole.ts
+++ middleware/verifyRole.ts
@@ -1,12 +1,9 @@
 import { Request, Response, NextFunction } from "express";
-import { User } from "../models/userModel.js";
 import { verifyAuth } from "./verifyAuth.js";
 
 const verifyRole = async (req: Request, res: Response, next: NextFunction) => {
   verifyAuth(req, res, async () => {
-    const user = await User.findById(req.user!.userId);
-
-    if (user!.role === "admin") {
+    if (req.user!.role === "admin") {
       next();
     } else {
       res.status(403).json({ msg: "User has no access permission" });
