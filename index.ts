2ec5b2ca4245e40b7c3690ae0bca46abad69bbaf
rhaix23 <patrick.lloyd.santos@gmail.com>
2022-12-30 01:24:04 +0800

    Admin Routes Added to the API

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ dotenv.config();
 // Import custom middleware
 import { errorHandler } from "./middleware/errorHandler.js";
 import { notFound } from "./middleware/notFoundHandler.js";
+import { verifyRole } from "./middleware/verifyRole.js";
 
 // Import database
 import { connectDB } from "./database/db.js";
@@ -24,15 +25,17 @@ import { subcategoryRouter } from "./routes/subcategoryRoutes.js";
 import { postRouter } from "./routes/postRoutes.js";
 import { commentRouter } from "./routes/commentRoutes.js";
 import { ratingRouter } from "./routes/ratingRoute.js";
+import { adminPostRouter } from "./routes/admin/adminPostRoutes.js";
+import { adminUserRouter } from "./routes/admin/adminUserRoutes.js";
+import { adminCategoryRouter } from "./routes/admin/adminCategoryRoutes.js";
+import { adminSubcategoryRouter } from "./routes/admin/adminSubcategoryRoutes.js";
+import { adminCommentRouter } from "./routes/admin/adminCommentRoutes.js";
+
+// Allowed origins configuration
+import { allowedOrigins } from "./configurations/allowedOrigins.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-const allowedOrigins = [
-  "https://forum-application.vercel.app",
-  "http://localhost:5173",
-  "http://localhost:3000",
-];
 
 // Middlewares
 if (process.env.NODE_ENV === "development") {
@@ -55,13 +58,20 @@ app.get("/", (req: Request, res: Response) => {
   res.send("API is running...");
 });
 
-// Routes
+// User routes
 app.use("/api/users", userRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/subcategory", subcategoryRouter);
 app.use("/api/post", postRouter);
 app.use("/api/comment", commentRouter);
 app.use("/api/rating", ratingRouter);
+
+// Admin routes
+app.use("/api/admin/category", verifyRole, adminCategoryRouter);
+app.use("/api/admin/subcategory", verifyRole, adminSubcategoryRouter);
+app.use("/api/admin/user", verifyRole, adminUserRouter);
+app.use("/api/admin/post", verifyRole, adminPostRouter);
+app.use("/api/admin/comment", verifyRole, adminCommentRouter);
 
 // Not found handler
 app.use(notFound);
